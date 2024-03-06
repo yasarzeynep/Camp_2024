fd9144eb0474312604f262bde585a8f8ad2b99f1
Zeynep Yaşar <zynpsema@gmail.com>
2024-03-06 17:31:55 +0300

    add v46-47

--- CSharpCourse/Inheritance/Program.cs
+++ CSharpCourse/Inheritance/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Customer  customer=new Customer();
+Person[] persons=new Person[3]
+{
+    new Customer { FirstName = "Engin" },
+    new Student { FirstName = "Derin" }, 
+    new Person(FirstName = "Salih")
+};
+
+foreach (var person in persons)
+{
+    Console.WriteLine(person.FirstName);
+}
+
+
+class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+}
+
+class Customer : Person
+{
+    public string City { get; set; }
+}
+
+class Student : Person
+{
+ public string Department { get; set; }
+}
