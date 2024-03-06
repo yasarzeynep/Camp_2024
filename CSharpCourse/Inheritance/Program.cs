9c8730b743e193ae7c18e814c50a6e53e5f8874b
Zeynep Yaşar <zynpsema@gmail.com>
2024-03-06 17:42:05 +0300

    fix bug

--- CSharpCourse/Inheritance/Program.cs
+++ CSharpCourse/Inheritance/Program.cs
@@ -1,9 +1,11 @@
-﻿Customer  customer=new Customer();
+﻿using System;
+
+Customer  customer=new Customer();
 Person[] persons=new Person[3]
 {
     new Customer { FirstName = "Engin" },
-    new Student { FirstName = "Derin" }, 
-    new Person(FirstName = "Salih")
+    new Student { FirstName = "Derin" } ,
+    new Person{FirstName="Salih"}
 };
 
 foreach (var person in persons)
@@ -11,17 +13,29 @@ foreach (var person in persons)
     Console.WriteLine(person.FirstName);
 }
 
+class Person2
+{
+
+}
+interface IPerson
+{
+
+}
 
 class Person
 {
+    //field 
+    //public int Id; burada field değil Property kullanılır.
+
+    //Property
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
 
 }
 
-class Customer : Person
-{
+class Customer : Person, IPerson // Person2 :bir kere inheritance alınır
+{                                //Once inheritance yazılır, sonra interfaceler yazılır
     public string City { get; set; }
 }
 
